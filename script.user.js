70584cdfcfb0da75a8f3f9b76cf9b655fa25257e
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-01-28 18:44:06 +0000

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @include     http://algerianmo.com/*
 // @downloadURL https://github.com/SpeedCode210/algerianmo-progress/raw/main/script.user.js
 // @icon http://www.algerianmo.com/static/images/favicon.ico
-// @version     1.2.2
+// @version     1.3
 // @author      Raouf Ould Ali / SpeedCode#0050
 // @description 1/25/2023, 5:34:04 PM
 // ==/UserScript==
@@ -19,11 +19,11 @@ if(window.location.href.match(/algerianmo.com\/problems\/(a|nt|g|b|c)\/(?![0-9])
 
   for(let i = 0; i < cards.length; i++){
     let header = cards[i].querySelector('.card-header');
-    if(header.style.backgroundColor == "rgb(7, 38, 15)" || header.style.backgroundColor == "rgb(159, 249, 156)")
+    if(header.style.backgroundColor == "rgb(7, 38, 15)" || header.style.backgroundColor == "rgb(159, 249, 156)"  || header.style.backgroundColor == "rgb(39, 79, 23)")
       goodOnes++;
-    if(header.style.backgroundColor == "rgb(96, 63, 1)" || header.style.backgroundColor == "rgb(255, 228, 105)" || header.style.backgroundColor == "rgb(253, 255, 163)")
+    if(header.style.backgroundColor == "rgb(96, 63, 1)" || header.style.backgroundColor == "rgb(243, 166, 12)" || header.style.backgroundColor == "rgb(255, 228, 105)" || header.style.backgroundColor == "rgb(253, 255, 163)")
       pendingOnes++;
-    if(header.style.backgroundColor == "rgb(86, 0, 0)" || header.style.backgroundColor == "rgb(237, 150, 158)")
+    if(header.style.backgroundColor == "rgb(86, 0, 0)" || header.style.backgroundColor == "rgb(99, 28, 28)" || header.style.backgroundColor == "rgb(237, 150, 158)")
       badOnes++;
   }
   title.innerHTML += " <span class='text-success' id='advancement'>"+goodOnes+"</span>"
@@ -32,6 +32,7 @@ if(window.location.href.match(/algerianmo.com\/problems\/(a|nt|g|b|c)\/(?![0-9])
     +"<span class='text-secondary' id='advancement'>.</span>"
     +"<span class='text-danger' id='advancement'>"+badOnes+"</span>"
     +"<span class='text-secondary' id='advancement'>/" + cards.length + "</span>";
+
 }
 
 else if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g)){
