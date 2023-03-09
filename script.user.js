c62331cc505a311b3d01a9ddb79b380c7cf20d5d
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-03-09 14:21:38 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,10 +4,18 @@
 // @include     *algerianmo.com/*
 // @downloadURL https://github.com/SpeedCode210/algerianmo-progress-display/raw/main/script.user.js
 // @icon http://www.algerianmo.com/static/images/favicon.ico
-// @version     1.6.1
+// @version     1.7.1
 // @author      Raouf Ould Ali / SpeedCode#0050
 // @description 1/25/2023, 5:34:04 PM
 // ==/UserScript==
+
+const teamWhiteProblems = [118,119,120,121,122,123,162,163,164,166,170,171,172,173,174,175,176,177,178,179,180,
+                           131,132,133,134,135,136,137,138,140,182,183,184,186,187,188,189,190,193,139,141,142,
+                           143,144,185,191,192,194,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,
+                           87,88,89,90,101,102,103,104,105,106,107,108,195,91,92,93,94,95,96,99,100,109,110,111,
+                           112,113,97,98,114,115,116,117,57,58,59,60,65,66,67,73,74,75,76,61,62,63,64,,68,69,77,
+                           78,79,80,81,82,83,85,70,71,72,84];
+
 document.getElementsByTagName('head')[0].innerHTML += `
 <style>
 .team-white{
@@ -42,6 +50,10 @@ if(mod){
 
   for(let i = 0; i < cards.length; i++){
     let header = cards[i].querySelector('.card-header');
+    let title = header.getElementsByTagName('a')[0];
+    if(teamWhiteProblems.includes(parseInt(title.innerHTML.match(/[0-9]+/g)[0]))){
+      title.innerHTML += `<span class="team-white">TEAM WHITE</span>`;
+    }
     if((now.getMonth() == (4) - 1 && now.getDate() == (1))){
        if(header.style.backgroundColor == "rgb(7, 38, 15)" || header.style.backgroundColor == "rgb(159, 249, 156)"  || header.style.backgroundColor == "rgb(39, 79, 23)")
       badOnes++;
@@ -80,6 +92,11 @@ else if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g)){
   let c = 0;
   let g = 0;
   let nt = 0;
+  let t_w = 0;
+  let a_w = 0;
+  let c_w = 0;
+  let g_w = 0;
+  let nt_w = 0;
   let problems = document.getElementsByTagName("a");
   for(let i = 0; i < problems.length; i++){
     let match = problems[i].href.match(/(?<=\/problems\/)(b|a|nt|c|g)(?=\/[0-9]+)/g);
@@ -91,6 +108,14 @@ else if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g)){
           case "g":g++;break;
           case "nt":nt++;break;
       }
+      if(teamWhiteProblems.includes(parseInt(problems[i].innerHTML.match(/[0-9]+/g)[0]))){
+        switch(match[0]){
+          case "a":a_w++;break;
+          case "c":c_w++;break;
+          case "g":g_w++;break;
+          case "nt":nt_w++;break;
+      }
+      }
     }
 
 
@@ -99,11 +124,20 @@ else if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g)){
     for(let i = 0; i < problems.length; i++){
     if(problems[i].innerHTML.match(/مسألة/g)){
       t++;
+      if(teamWhiteProblems.includes(parseInt(problems[i].innerHTML.match(/[0-9]+/g)[0]))){
+        t_w++;
+      }
     }
     }
 
   document.getElementsByClassName("row")[0].children[2].innerHTML += `
+  <h6 style='text-align:center!important;'>احصائيات عامة</h3><br>
   <p style="margin-top:10px;background: black;color: white;padding: 5px;border-radius: 5px;">الأساسيات : %${Math.floor(100*b/31)} - الجبر : %${Math.floor(100*a/73)} - التوفيقات : %${Math.floor(100*c/45)} <br> الهندسة : %${Math.floor(100*g/41)} - نظريات الأعداد : %${Math.floor(100*nt/94)} - المجموع : ${t}</p>
+  `;
+
+  document.getElementsByClassName("row")[0].children[2].innerHTML += `
+  <h6 style='text-align:center!important;'>Team White Instructions</h3><br>
+  <p style="margin-top:10px;background: white;color: black;padding: 5px;border-radius: 5px;">الجبر : %${Math.floor(100*a_w/48)} - التوفيقات : %${Math.floor(100*c_w/32)} - الهندسة : %${Math.floor(100*g_w/22)} <br> نظريات الأعداد : %${Math.floor(100*nt_w/29)} - المجموع : ${t_w}/${teamWhiteProblems.length}</p>
   `;
 
 
