e0599c812f991dc5092c32c4831af55e034533e1
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-03-09 14:29:42 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,14 +4,14 @@
 // @include     *algerianmo.com/*
 // @downloadURL https://github.com/SpeedCode210/algerianmo-progress-display/raw/main/script.user.js
 // @icon http://www.algerianmo.com/static/images/favicon.ico
-// @version     1.7.1
+// @version     1.7.2
 // @author      Raouf Ould Ali / SpeedCode#0050
 // @description 1/25/2023, 5:34:04 PM
 // ==/UserScript==
 
 const teamWhiteProblems = [118,119,120,121,122,123,162,163,164,166,170,171,172,173,174,175,176,177,178,179,180,
                            131,132,133,134,135,136,137,138,140,182,183,184,186,187,188,189,190,193,139,141,142,
-                           143,144,185,191,192,194,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,
+                           143,144,191,192,194,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,
                            87,88,89,90,101,102,103,104,105,106,107,108,195,91,92,93,94,95,96,99,100,109,110,111,
                            112,113,97,98,114,115,116,117,57,58,59,60,65,66,67,73,74,75,76,61,62,63,64,,68,69,77,
                            78,79,80,81,82,83,85,70,71,72,84];
@@ -37,7 +37,7 @@ let mod = window.location.href.match(/(?<=algerianmo.com\/problems\/)(a|nt|g|b|c
 if(mod){
   let whiteValue = "";
   switch(mod[0]){
-      case "a": whiteValue = "48";break;
+      case "a": whiteValue = "47";break;
       case "g": whiteValue = "22";break;
       case "nt": whiteValue = "29";break;
       case "c": whiteValue = "32";break;
